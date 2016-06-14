63ff9155d2371a2d7473781f7a280d5811de3d81
ChesapeakeBayProject <thouvbs@gmail.com>
2016-06-14 17:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#input_h3_1').click(function() {
 		var input_val = prompt("In how many years do you want the Bay cleaned with its current pollution level and its current pollution rate?");
 		var output_val_l = 15000000000000 / input_val / 17500;
-		console.log(output_val_l);
+		$('.output').prepend("<li>Testing" + output_val_l + "Test</li>")
 	});
 	$('#input_h3_2').click(function() {
 		var input_val = prompt("In how many years do you want the Bay cleaned with its current pollution level and no further pollution?");
