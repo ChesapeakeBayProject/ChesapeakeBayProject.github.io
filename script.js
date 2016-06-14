161b5564fd8a9658e2dc1e27d2d1af1a07d3df03
ChesapeakeBayProject <thouvbs@gmail.com>
2016-06-14 19:34:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var w_output_val_l_2 = w_output_val_l_1 + ((300000000 * input_val) / input_val / 17500);
 		var w_output_val_h_1 = 18000000000000 / input_val / 17500;
 		var w_output_val_h_2 = w_output_val_h_1 + ((12000000 * input_val) / input_val / 17500);
-		$('.output').prepend("<li class="outputs">Considering the density of nitrogen in the Chesapeake bay is 5mg/L of water, it would take between " + w_output_val_l_2 + " and " + w_output_val_h_2 + " oysters " + input_val + " years to clean the Bay completely with its current amount of pollution and its continued rate of pollution.</li>")
+		$('.output').prepend("<li>Considering the density of nitrogen in the Chesapeake bay is 5mg/L of water, it would take between " + w_output_val_l_2 + " and " + w_output_val_h_2 + " oysters " + input_val + " years to clean the Bay completely with its current amount of pollution and its continued rate of pollution.</li>")
 	});
 	$('#input_h3_2').click(function() {
 		var input_val = prompt("In how many years do you want the Bay cleaned with its current pollution level and no further pollution?");
